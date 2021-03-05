933778c4a2327dff1604494179adef19fb90e46e
Austin Bravo <austin@nationdigital.io>
2021-03-05 16:28:10 -0600

    Add initial test

---
+++ tests/mock.py
@@ -0,0 +1,7 @@
+json_dict = {
+    "attr": "attr value",
+    "nested": {"attr": "nested attr value"},
+    "list_attr": [1, 2, 3],
+    "nested_list": {"list_attr": [1, 2, 3]},
+    "nested_attr_list": [{"attr": "in list"}],
+}
